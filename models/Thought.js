d024446305bfa44435c8e3718a8f5ceb3f3b81e4
jkoufalas <john@koufalas.com>
2022-10-05 11:38:05 +1030

    initial commit and models

---
+++ models/Thought.js
@@ -0,0 +1,58 @@
+const { Schema, model } = require("mongoose");
+const Reaction = require("./Reaction");
+
+// Schema to create Post model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: formatDate,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+
+    reactions: [Reaction],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+thoughtSchema
+  .virtual("reactionCount")
+  // Getter
+  .get(function () {
+    return `${this.reactions.length()}`;
+  });
+
+function formatDate(createdAt) {
+  return (
+    [
+      createdAt.getDate(),
+      createdAt.getMonth() + 1,
+      createdAt.getFullYear(),
+    ].join("/") +
+    " " +
+    [createdAt.getHours(), createdAt.getMinutes(), createdAt.getSeconds()].join(
+      ":"
+    )
+  );
+}
+
+// Initialize our Video model
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,66 @@
+const { Schema, model } = require("mongoose");
+
+const validateEmail = (email) => {
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(email);
+};
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: true,
+      // validate is used on creation
+      validate: [validateEmail, "Please fill a valid email address"],
+      //match is used to validate when save is used
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+userSchema
+  .virtual("friendCount")
+  // Getter
+  .get(function () {
+    return `${this.friends.length()}`;
+  });
+
+// Initialize our User model
+const User = model("user", userSchema);
+
+module.exports = User;
