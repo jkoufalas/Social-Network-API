be29bc041f178257630c6acae371e3001207d7e5
jkoufalas <john@koufalas.com>
2022-10-06 13:25:34 +1030

    added routes for users gets and create

---
+++ controllers/userController.js
@@ -0,0 +1,35 @@
+const User = require("../models/User");
+const Thought = require("../models/Thought");
+
+module.exports = {
+  getUsers(req, res) {
+    console.log(`got here`);
+    User.find()
+      .select("-__v")
+      .populate("thoughts")
+      .populate("friends")
+      .then((users) => res.json(users))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .populate("thoughts")
+      .populate("friends")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
+  },
+};
--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,20 @@
 const { Schema, model } = require("mongoose");
 const Reaction = require("./Reaction");
 
+function formatDate(createdAt) {
+  return (
+    [
+      createdAt.getDate(),
+      createdAt.getMonth() + 1,
+      createdAt.getFullYear(),
+    ].join("/") +
+    " " +
+    [createdAt.getHours(), createdAt.getMinutes(), createdAt.getSeconds()].join(
+      ":"
+    )
+  );
+}
+
 // Schema to create Post model
 const thoughtSchema = new Schema(
   {
@@ -31,26 +45,12 @@ const thoughtSchema = new Schema(
 );
 
 // Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
-thoughtSchema
+/* thoughtSchema
   .virtual("reactionCount")
   // Getter
   .get(function () {
     return `${this.reactions.length()}`;
-  });
-
-function formatDate(createdAt) {
-  return (
-    [
-      createdAt.getDate(),
-      createdAt.getMonth() + 1,
-      createdAt.getFullYear(),
-    ].join("/") +
-    " " +
-    [createdAt.getHours(), createdAt.getMinutes(), createdAt.getSeconds()].join(
-      ":"
-    )
-  );
-}
+  }); */
 
 // Initialize our Video model
 const Thought = model("thought", thoughtSchema);
